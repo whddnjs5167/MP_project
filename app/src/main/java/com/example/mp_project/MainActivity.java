3063594fb9bcdf6c1445e95ba7baff22563ba1de
whddnjs5167 <whddnjs5167@naver.com>
2022-05-19 12:42:36 +0900

    writhe firebase

--- app/src/main/java/com/example/mp_project/MainActivity.java
+++ app/src/main/java/com/example/mp_project/MainActivity.java
@@ -6,20 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button  btn1;
+
+    TextView readText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        readText = findViewById(R.id.temperatureText);
+
         btn1 = findViewById(R.id.button);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
+            //Change VideoActivity
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), VideoActivity.class);
                 startActivity(intent);
