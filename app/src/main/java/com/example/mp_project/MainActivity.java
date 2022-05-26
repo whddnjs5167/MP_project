dd7aa77b821ce5b8ddcad8973d29e3f8017dce76
whddnjs5167 <whddnjs5167@naver.com>
2022-05-26 15:07:18 +0900

    add test value send func

--- app/src/main/java/com/example/mp_project/MainActivity.java
+++ app/src/main/java/com/example/mp_project/MainActivity.java
@@ -6,30 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+//import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     Button  btn1;
-
-    TextView readText;
+    Button senddata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        readText = findViewById(R.id.temperatureText);
+        //firebase data insert
+        senddata = findViewById(R.id.button2);
 
+        senddata.setOnClickListener(v -> {
+            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+            DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+            databaseReference.child("users").setValue("2");
+            //message
+            Toast t = Toast.makeText(getApplicationContext(), "사료주기 완료", Toast.LENGTH_SHORT);
+            t.show();
+            //
+        });
+        //
+
+        //activity change
         btn1 = findViewById(R.id.button);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            //Change VideoActivity
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), VideoActivity.class);
-                startActivity(intent);
-            }
+        //Change VideoActivity
+        btn1.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), VideoActivity.class);
+            startActivity(intent);
         });
     }
+
 }
